dda867fced3179668b867fd5378b1a7c85c2ed49
Rose <rose.lin.swe@gmail.com>
2019-10-31 01:11:36 -0400

    Update navbar to match route path

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
 import Background from "./components/background/Background";
 import NavbarPage from './components/navbar/Navbar';
+import { BrowserRouter as Router, Route, Switch, withRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
     <MDBContainer fluid>
       <Background />
+      
+      <Router>
       <NavbarPage />
+      </Router>
       {/* <MDBRow>
         <MDBCol>One of three columns</MDBCol>
         <MDBCol>One of three columns</MDBCol>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,70 +1,68 @@
-import React, { Component } from "react";
-import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBDropdown,
-MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBIcon } from "mdbreact";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { boundMethod } from 'autobind-decorator'
+import React, { Component, Fragment } from "react";
+import { boundMethod } from 'autobind-decorator';
+import { BrowserRouter as Router, Route, Switch, withRouter } from 'react-router-dom';
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBCollapse } from "mdbreact";
 
 import Home from "../home/Home";
 
 class NavbarPage extends Component {
-state = {
+  static ROUTES = [
+    {
+      page: "/",
+      text: "about"
+    },
+    {
+      page: "/jobs",
+      text: "experience"
+    },
+    {
+      page: "/skills",
+      text: "skills"
+    },
+    {
+      page: "/contact",
+      text: "contact"
+    },
+  ]
+
+  // static 
+
+  state = {
     activePage: "/"
+  }
+
+componentDidMount() {
+  this.setState({activePage: this.props.location.pathname});
 }
 
-@boundMethod
-updateActivePage(path) {
-    console.log('dfkjsfklsjdkfsdf', this);
-    this.setState({activePage: path});
+componentWillReceiveProps(props) {
+  this.setState({activePage: props.location.pathname});
 }
 
 render() {
   return (
-    <Router>
+    <Fragment>
       <MDBNavbar color="default-color" dark expand="md">
         <MDBNavbarBrand>
           <strong className="white-text">Rose Lin | Software Engineer </strong>
         </MDBNavbarBrand>
-
         <MDBCollapse id="navbarCollapse3" navbar>
           <MDBNavbarNav right>
-            <MDBNavItem active={this.state.active="/"}>
-              <MDBNavLink to="/">About</MDBNavLink>
+          {NavbarPage.ROUTES.map(({page, text}) => (
+            <MDBNavItem key={text} active={this.state.activePage === page}>
+              <MDBNavLink to={page}>{text}</MDBNavLink>
             </MDBNavItem>
-            <MDBNavItem>
-              <MDBNavLink to="jobs">Experience</MDBNavLink>
-            </MDBNavItem>
-            <MDBNavItem>
-              <MDBNavLink to="skills">Skills</MDBNavLink>
-            </MDBNavItem>
-            <MDBNavItem>
-            <MDBNavLink to="contact">Contact</MDBNavLink>
-            </MDBNavItem>
+          ))}
           </MDBNavbarNav>
         </MDBCollapse>
       </MDBNavbar>
-
-
       <Switch>
-          {/* <h1>{JSON.stringify(window.location.pathname)}</h1> */}
-      <Route exact path="/" component={Home} updateActivePage={this.updateActivePage}/>
-      <Route exact path="/jobs" render={(props) => <Home {...props} updateActivePage={this.updateActivePage} />} />
-          <Route path="/skills">
-          <h1>SKILLS</h1>
-          </Route>
-          <Route path="/contact">
-          <h1>CONTACT</h1>
-          </Route>
-
-        </Switch>
-    </Router>
+       <Route exact path="/" component={Home} />
+       <Route exact path="/jobs" component={() => <h1>LOL</h1>} />
+      </Switch>
+     </Fragment>
     );
   }
 }
 
-export default NavbarPage;
-
-
-{/* <MDBIcon fab icon="linkedin" />
-<MDBIcon fab icon="github-square" />
-<MDBIcon icon="envelope" />
-<MDBIcon icon="file" /> */}
+export default withRouter(NavbarPage);
