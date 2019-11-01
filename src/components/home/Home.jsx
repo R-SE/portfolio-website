15c4b4a8330a3f80f96d1f3751b78d014b5c79fb
Rose <rose.lin.swe@gmail.com>
2019-11-01 18:59:19 -0400

    Tweak transition to include scaling

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 const Home = () => {
   const [idx, setIdx] = useState(0);
-  const [opacity, setOpacity] = useState(1);
+  const [style, setStyle] = useState({opacity: 1, transform: "scale(1)"});
 
   const strings = [
     ["I build performant, scalable web apps", <br/>, "and deliver clean, well-designed code"],
@@ -13,22 +13,22 @@ const Home = () => {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setOpacity(opacity => Number(!opacity));
-      setTimeout(() => setOpacity(opacity => Number(!opacity)), 600);
+      setStyle(() => ({opacity: 0, transform: "scale(0.95)"}));
+      setTimeout(() => setStyle(() => ({opacity: 1, transform: "scale(1)"})), 600);
       setTimeout(() => setIdx(idx => (idx + 1) % strings.length), 300);
     }, 5000);
     return () => clearInterval(interval);
   }, [strings.length]);
 
   return (
-  <div id="home">
-    <h1>I'm <span role="img" aria-label="Rose" title="Rose">🌹</span>.</h1>
-    <h4 style={{opacity: opacity}}>{strings[idx]}</h4>
-    <div className="scroll-div">
-      <span>Scroll</span>
-      <img src="./assets/double_arrow_down.png" alt="down arrows" className="directional-arrow"/>
-    </div>
-</div>
+    <div id="home">
+      <h1>I'm <span role="img" aria-label="Rose" title="Rose">🌹</span>.</h1>
+      <h4 style={style}>{strings[idx]}</h4>
+      <div className="scroll-div">
+        <span>Scroll</span>
+        <img src="./assets/double_arrow_down.png" alt="down arrows" className="directional-arrow"/>
+      </div>
+  </div>
 )};
 
 export default Home;
